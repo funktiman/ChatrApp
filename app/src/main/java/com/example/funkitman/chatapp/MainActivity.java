655e3efc78a81dcaf74bed1bcec03ddc9845fd42
funkitman <sundeka@hotmail.com>
2019-04-23 23:09:59 +0200

    test3

--- app/src/main/java/com/example/funkitman/chatapp/MainActivity.java
+++ app/src/main/java/com/example/funkitman/chatapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    public string navn "";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
